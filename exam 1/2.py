e8b9c978fe4fca7e0f795bf961b0e6a716990b15
ranjuinrush <niranjannidugala@gmail.com>
2018-07-30 16:39:55 +0530

    Add files via upload

--- exam 1/2.py
+++ exam 1/2.py
@@ -1,47 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jul 30 15:58:12 2018
+Created on Mon Jul 30 16:30:20 2018
 
 @author: user
 """
-
-diction={}
-for i in range(10):
-    name=str(input("Enter the name:"))
-    marks=str(input("Enter the marks:"))
-    diction[name]=marks
-import operator
-sort=sorted(diction.items(),key=operator.itemgetter(1))
-print("result")
-print('("Name","Marks")')
-print(sort)
-
-name1=[]
-m1=[]
-m2=[]
-m3=[]
-for i in range(0,2):
-    name=input("Enter the name:")
-    sub1=int(input("Enter marks of the first subject: "))
-    sub2=int(input("Enter marks of the second subject: "))
-    sub3=int(input("Enter marks of the third subject: "))
-    name1=name1.append(name)
-    m1=m1.append(sub1)
-    m2=m2.append(sub2)
-    m3=m3.append(sub3)
-    
-print(name1,m1,m2,m3)
-
-avg=(sub1+sub2+sub3)/3
-if(avg>=90):
-    print("Grade: A+")
-elif(avg>=80&avg<90):
-    print("Grade: A")
-elif(avg>=70&avg<80):
-    print("Grade: B+")
-elif(avg>=60&avg<70):
-    print("Grade: B")
-elif(avg>=50&avg<60):
-    print("Grade: C")
-else:
-    print("Grade: D")
+name=[""]
+m1=[""]
+m2=[""]
+m3=[""]
+n=int(input("Enter number of students:"))
+for i in range(1,n+1):
+    b=(input("Enter name:")
+    name.append(b)
+    a=(input("Enter sub 1:"))
+    m1.append(a)
+    c=(input("Enter sub 2:"))
+    m2.append(c)
+    d=(input("Enter sub 3:"))
+    m3.append(d)
+for i in range(1,n+1):
+    print (name,m1,m2,m3)
